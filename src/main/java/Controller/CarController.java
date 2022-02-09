6a200a3f7feda56870f555181730619cccd089cc
simeonlead <33431401+simeonlead@users.noreply.github.com>
2022-02-09 23:54:47 +0100

    CarControllerUpdate

--- src/main/java/Controller/CarController.java
+++ src/main/java/Controller/CarController.java
@@ -3,10 +3,7 @@ package Controller;
 import Entity.Car;
 import Service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +16,28 @@ public class CarController {
     @PostMapping("/addCar")
     public Car addCar(@RequestBody Car car){
          return  service.addCar(car);
+    }
 
-        }
+    //Add Cars to the Databse
+    @PostMapping("/addCars")
     public List<Car> addCars(@RequestBody List<Car> cars){
         return  service.addCars(cars);
-
     }
+    // Populate All Cars in the Database
+    @GetMapping("/cars")
+    public  List<Car> findAllCars(){
+        return service.getCars();
+    }
+    // find individual car using vin
+    @GetMapping("/cars/{vin}")
+    public Car getCarByvin(@PathVariable String vin){
+        return service.getCarByvin(vin);
+    }
+
+    // Update Car detail
+    @PutMapping("/updateCar")
+    public Car upadateCar(@RequestBody Car car){
+        return  service.updateCar(car);
+    }
+
 }
